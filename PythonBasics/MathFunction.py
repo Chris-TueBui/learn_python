6f884763cf5ff655ebd35385404d9fecee9ffcbb
Chris-TueBui <chris.tuebui@gmail.com>
2024-07-30 21:07:30 -0400

    Python Basics part 1

---
+++ PythonBasics/MathFunction.py
@@ -0,0 +1,5 @@
+# round up/down based on the round number rule.
+print(round(7.9)) # round up
+print(round(3.2)) # round down
+
+print(abs(-9.0))
---
+++ PythonBasics/data_types.py
@@ -0,0 +1,21 @@
+# Fundamental data type:
+#int, float, str bool, list, tuple, set, dict.
+
+#Clases -> Custom types
+
+#Specialize data types (special packages, modules, etc.)
+#None
+
+print(2+4)
+print(2-4)
+print(2*4)
+print(2/4)
+print(2%4)
+print(2 ** 3) #2 to the power of 3
+print(2 // 4) #round down the answer of division to int.
+
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))
+print(type(2%4))
