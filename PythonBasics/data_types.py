7e247be31cafb5c8d5b21404474654cee38bb951
Chris-TueBui <chris.tuebui@gmail.com>
2024-08-01 21:03:23 -0400

    Intro to python 2

--- PythonBasics/data_types.py
+++ PythonBasics/data_types.py
@@ -19,3 +19,31 @@ print(type(2-4))
 print(type(2*4))
 print(type(2/4))
 print(type(2%4))
+
+##Complex data type. Beside int or flaot, there is complex too.
+## BIN() returns binary number of the parameter
+print(bin(5)) ## the answer contains 0b which indicates the binary number
+# print(bin(10.5)) ##only integer
+print(int('0b101', 2)) ## ,2 means base 2
+
+# A str is simple a piece of text
+'hi hello there!!!' # '' or "" is fine
+print(type("hi hello there!!!"))
+username = "supercode"
+password = "supercoder"
+
+longString = '''
+WOW, 
+THIS IS
+A LONG
+STRING
+'''
+
+print(longString)
+
+# STRING CONCATENATION
+print("hello " + " Chris")
+# Cant do print("hello " + 5). String cant be operated with integer
+
+print(type(str(500))) # -> str
+print(type(int(str(500)))) # -> int
---
+++ PythonBasics/variable.py
@@ -0,0 +1,27 @@
+#variable
+iq = 190
+print(iq)
+
+##NAMING CONVENTION
+## snake_case
+# start with lowercase or udnerscore
+# letters, numbers, underscores
+# case sensitive
+# dont overwrite keywords
+# note: underscore indicates private variable
+
+#CONSTANTS: variable never change. In capital.
+# __ indicates dunder: They are defined by built-in classes in Python and commonly used for operator overloading. 
+a, b, c = 1, 2, 3
+# a = 1, b = 2, c = 3
+
+#Augmented assignment operator
+some_value = 5
+some_value += 2
+print(some_value)
+some_value -= 2
+print(some_value)
+some_value *= 2
+print(some_value)
+some_value /= 2
+print(some_value)
